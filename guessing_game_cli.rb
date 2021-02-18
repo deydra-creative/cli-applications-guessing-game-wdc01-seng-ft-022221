73a73aa574636622785cf78d57970bde0c70af8f
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-18 02:35:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,2 +1,16 @@
 def run_guessing_game
-p rand(1..6)
+
+
+
+  num = rand(6)+1
+  user_input = gets.chomp.to_i
+
+  if user_input == 'exit'
+    puts "Goodbye!"
+  end
+
+  if user_input == num
+    print "You guessed the correct number!"
+  else
+    print "Sorry! The computer guessed #{num}."
+  end
