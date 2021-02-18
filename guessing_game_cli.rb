0c2f37bffc07de67c1c53483d9d348f2a18d2764
Deydra Cavazos <deydra@cavazospartners.com>
2021-02-18 03:05:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,9 @@ def run_guessing_game
 num = rand(6)+1
 user_input = gets.chomp.to_i
 
-if user_input =='exit'
-  print "Goodbye!"
-end
+if user_input == 'exit'
+    puts "Goodbye!"
+  end
 
 if user_input == num
     print "You guessed the correct number!"
